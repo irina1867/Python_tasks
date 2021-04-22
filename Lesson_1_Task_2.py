a68776347be9af2fa38cb06df8f637bf09cfbb0e
irina1867 <82774778+irina1867@users.noreply.github.com>
2021-04-22 21:07:17 +0300

    Lesson_1_Task2

--- Lesson_1_Task_2.py
+++ Lesson_1_Task_2.py
@@ -1,6 +1,9 @@
-a = int(input("ввведите число"))
-print(a)
-s = input("ввведите строку")
-print(s)
-p = 3.14
-print(p ** 2)
+#2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. 
+# Используйте форматирование строк.
+
+seconds = int(input())
+# seconds = seconds % (3600 * 24)
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+clear_sec = seconds % 60
+print(f'{hours:02d}:{minutes:02d}:{clear_sec:02d}')
