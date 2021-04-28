62645e6a837ec8deb29cc771326bb1b89c3c7663
irina1867 <82774778+irina1867@users.noreply.github.com>
2021-04-28 20:38:56 +0300

    Lesson_1_Tasks

--- Task_4_Lesson_1.py
+++ Task_4_Lesson_1.py
@@ -1,18 +1,9 @@
 # Пользователь вводит целое положительное число. Найдите самую большую цифру в числе. Для решения используйте цикл while
 # и арифметические операции
-print("Введите ваше число")
-n = int(input())
-numb = n
-max = n % 10
-if n < 10:
-    print(n)
-else:
-    while n >= 1:
-        n = n // 10
-        if n % 10 > max:
-            max = n % 10
-        if n > 9:
-            continue
-        else:
-            print(max)
-            break
+n = int(input("Введите ваше число"))
+max = -1
+while n > 0:
+    if n % 10 > max:
+        max = n % 10
+    n = n // 10
+print(max)
