589613feac8879aaf5c5bc4eefc9943a12a88a8a
irina1867 <82774778+irina1867@users.noreply.github.com>
2021-05-06 21:13:08 +0300

    Lesson_2
    Lesson_2

--- Task_2_Lesson_2.py
+++ Task_2_Lesson_2.py
@@ -2,20 +2,9 @@
 # 3 и т.д. При нечетном количестве элементов последний сохранить на своем месте. Для заполнения списка элементов
 # необходимо использовать функцию input().
 
-lng =int(input("Введите количество элементов списка: "))
-i = 0
-user_list = []
+user_list = input("Введите список:").split()
 helper = 0
-while i < lng:
-    user_list.append(input("Введите элемент списка"))
-    i += 1
-i = 0
 print(user_list)
-for helper in range(int(len(user_list)/2)):
+for i in range(0, len(user_list)-1, 2):
     user_list[i], user_list[i + 1] = user_list[i + 1], user_list[i]
-    i += 2
 print(user_list)
-
-
-
-
