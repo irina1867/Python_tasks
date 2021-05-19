147e95ae9d7e36f6a453cc953227a36fb216d6fa
irina1867 <82774778+irina1867@users.noreply.github.com>
2021-05-19 22:23:04 +0300

    Lesson_3.

--- Lesson_3/Task_2_Lesson_3.py
+++ Lesson_3/Task_2_Lesson_3.py
@@ -2,13 +2,21 @@
 # имя, фамилия, год рождения, город проживания, email, телефон.
 # Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.
 
-name = input('your name')
-surname = input('enter surname')
-year = int(input('enter year'))
-city = input('enter city')
-email = input('enter email')
-telephone = input('input telephone')
+def my_func(name, surname, year, city, email, telephone):
+    return ' '.join([name, surname, str(year), city, email, telephone])
 
-def my_func (name, surname, year, city, email, telephone):
-     return ' '.join([name, surname, year, city, email, telephone])
-print(my_func(surname = 'Ozerova', name = 'Irina', year = '1881', city = 'Moscow', email = 'irina@gmail.com', telephone = '89031045195'))
+
+name_ = input('your name')
+surname_ = input('enter surname')
+year_ = int(input('enter year'))
+city_ = input('enter city')
+email_ = input('enter email')
+telephone_ = input('input telephone')
+
+print(my_func(
+    city=city_,
+    surname=surname_,
+    name=name_,
+    year=year_,
+    email=email_,
+    telephone=telephone_))
